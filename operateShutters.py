45da43919163dfd7f344a4180a6c7a9189548611
lfliess <lfliess@gmail.com>
2020-08-10 00:35:21 +0300

    pigpio in upper dir

--- operateShutters.py
+++ operateShutters.py
@@ -7,7 +7,7 @@ import os
 import locale
 import time
 import datetime
-import pigpio
+from .. import pigpio
 import socket
 import signal, atexit, traceback
 import logging, logging.handlers
@@ -411,7 +411,7 @@ class operateShutters(MyLog):
            status, process = subprocess.getstatusoutput('sudo pidof pigpiod')
            if status:  #  it wasn't running, so start it
                self.LogInfo ("pigpiod was not running")
-               subprocess.getstatusoutput('sudo pigpiod -l -m')  # try to  start it
+               subprocess.getstatusoutput('sudo ../pigpiod -l -m')  # try to  start it
                time.sleep(0.5)
                # check it again
                status, process = subprocess.getstatusoutput('sudo pidof pigpiod')
